3f4ec0d6fd1f1141783812a2ffb3e47208d78c54
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-07-07 23:25:08 +0530

    Initial Commit.

---
+++ src/test/java/com/lahiru/emsbackendapi/EmsBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahiru.emsbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

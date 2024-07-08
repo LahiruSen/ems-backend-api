a07203b6abe5f3e9a91a33d11449d0256ae65d0c
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-07-08 23:30:50 +0530

    Add employee entity, DTO, repository and mapper.

---
+++ src/main/java/com/lahiru/emsbackendapi/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.lahiru.emsbackendapi.mapper;
+
+import com.lahiru.emsbackendapi.dto.EmployeeDto;
+import com.lahiru.emsbackendapi.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getFirstName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
